8a7918a44a539364dd0acaa14359d3b93d8464e6
StoneStoneStone <zhangleijay@vip.qq.com>
2015-08-21 11:47:54 +0800

    t

--- src/com/stone/TypeOnWeb.java
+++ src/com/stone/TypeOnWeb.java
@@ -3,5 +3,6 @@ package com.stone;
 public class TypeOnWeb {
     public static void main(String[] args) {
         System.out.println("This file was created online(github) and code in eclipse");
+        
     }
 }
