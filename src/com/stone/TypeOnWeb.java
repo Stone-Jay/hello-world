0d71ad91b2c5d9d83b1cfaca148409f4e0bd8a4f
StoneStoneStone <zhangleijay@vip.qq.com>
2015-08-21 14:43:11 +0800

    Code TypeOnWeb
    type some codes in TypeOnWeb.java

--- src/com/stone/TypeOnWeb.java
+++ src/com/stone/TypeOnWeb.java
@@ -1,1 +1,8 @@
+package com.stone;
 
+public class TypeOnWeb{
+    public static void main(String[] args) {
+        System.out.println("created on web");
+        System.out.println("code in eclipse");
+    }
+}
